c1e7110c0f52672e1552d6978e09b36b98a73fb9
juanda22x <juan.castillor@upb.edu.co>
2025-04-28 07:42:30 -0500

    luzz

--- Assets/Sesiones/Juan David Castillo/ejLighting/Shader/include/code/Lighting.hlsl
+++ Assets/Sesiones/Juan David Castillo/ejLighting/Shader/include/code/Lighting.hlsl
@@ -1,5 +1,27 @@
+#ifndef LIGHTING_CUSTOM_INCLUDED
+#define LIGHTING_CUSTOM_INCLUDED
 
-void GetMainLight_float(out float3 lightDirection)
+
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+void GetMainLight_float(float3 PositionWS,out half3 Color,out float3 Direction, out float ShadowAttenuation)
 {
-    lightDirection = normalize(float3(1, 1, -1));
+    #if defined(SHADERGRAPH_PREVIEW)
+    Color = 1;
+    Direction = normalize(float3(1, 1, -1));
+    ShadowAttenuation=1;
+    #else
+    Light light = GetMainLight(shadowCoord);
+    float4 shadowcoord = TransformWorldToShadowCoord(PositionWS);
+    Color = light.color;
+    Direction = light.direction;
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    float shadowIntensity = GetMainLightShadowStrength();
+    
+    ShadowAttenuation = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexutre,sampler _MainLightShadowmapTexutre),shadowSamplingData, shadowIntensity, false);
+    
+    
+    #endif
 }
+
+#endif 
