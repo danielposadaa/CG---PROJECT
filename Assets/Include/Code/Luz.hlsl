ab2dc90d0405eb3ba500e586f1b8b54b97df4f59
Alejandroguerra4915 <alejandro.guerrab@upb.edu.co>
2025-05-02 07:37:07 -0500

    commit luces adicionales

--- Assets/Include/Code/Luz.hlsl
+++ Assets/Include/Code/Luz.hlsl
@@ -3,12 +3,12 @@
 //#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 void GetMainLight_float(float3 PositionWS, out half3 color, out float3 Direction, out float shadowAttenuation)
 {
-    #if defined(SHADERGRAPH_PREVIEW)
+    /*#if defined(SHADERGRAPH_PREVIEW)
     color = 1;
     Direction = normalized (float3 (1, 1, - 1));
     Shadowattenuation = 1;
     #else
-    Light light = GetMainLight(shadowCoord);
+    Light light = GetMainLight(shadowcoord);
     float4 shadowcoord = TranformWorldToShadowCoord(PositionWS);
     
     color = light.color;
@@ -18,8 +18,46 @@ void GetMainLight_float(float3 PositionWS, out half3 color, out float3 Direction
    
     shadowAttenuation = SampleShadowmap(shadowcoord, TEXTURE2D_ARGS(_MainLightShadowTexture, sample_MainLightShadowmapTexture),
     shadowSamplingData,
-    shadowintensity, false);
+    shadowintensity, false);*/
+   
     #endif 
 }
 
-#endif 
+
+ 
+    void ComputeLightForAdditionalLights_float(float3 positionWS, float3 normalWS, float3 viewDirectionWS,  out float3 Finalcolor)
+    {
+    
+        #if defined(SHADERGRAPH_PREVIEW)
+        Finalcolor = 0
+        #else
+        int lightcount = GetAdditionalLightCount();
+    
+        half3 lighting = 0;
+    
+        [unroll(8)]
+        for (int lightid = 0;lightid < lightcount; lightid++)
+        {
+            light = GetAdditionalLight(lightid, positionWS);
+        
+        half lambert = dot(normalWS, light.direction);
+       
+        float3 h = normalize(lightcount.direction + viewDirectionWS);
+        half NoH = dot(normalWS, h);
+        half blingphonk = pow(saturate(NoH), 50.0f);
+        
+        
+        half diffuselightin = light.color(lambert * light.shadowAttenuation * light.DistanceAttenuation);
+        half specularLighting = light.color(blingphonk * light.shadowAttenuation * light.distanceAttenuation);
+        
+        lighting += disffuselighting + specularLighting;
+           
+        }
+
+        Finalcolor = lighting;
+        #endif 
+    }
+
+  #endif 
+
+
